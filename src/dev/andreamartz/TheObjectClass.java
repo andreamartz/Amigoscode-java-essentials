ad5ebfcdc5f78c5e6f4bf3c751af4124a20e3aa1
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-07-30 18:17:43 -0500

    The Object Class

---
+++ src/dev/andreamartz/TheObjectClass.java
@@ -0,0 +1,11 @@
+package dev.andreamartz;
+
+public class TheObjectClass {
+    public static void main(String[] args) {
+        // The Object class is the root of all other classes
+        // Every other class can use all the same methods that the Object class has on it
+
+        Object name = "Jamil";  // or we could have said String name = "Jamil"
+        System.out.println(name);
+    }
+}
