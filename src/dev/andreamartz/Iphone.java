437174da6328325d78d31e33f8339d1cbd9568d0
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-07-31 12:35:11 -0500

    Classes and Behaviors - Iphone class

--- src/dev/andreamartz/Iphone.java
+++ src/dev/andreamartz/Iphone.java
@@ -7,4 +7,32 @@ public class Iphone {
     private String model;
     private double price;
     private boolean isWaterResistant;
+
+
+    // the Iphone `behaviors`:
+
+    // Getters and Setters ( ^ Enter )
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isWaterResistant() {
+        return isWaterResistant;
+    }
+
+    public void setWaterResistant(boolean waterResistant) {
+        this.isWaterResistant = waterResistant;
+    }
 }
