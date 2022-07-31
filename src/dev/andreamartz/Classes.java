8085b16ce208964a299c1345e26dfa654ed1b79d
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-07-31 13:19:02 -0500

    Creating Objects

--- src/dev/andreamartz/Classes.java
+++ src/dev/andreamartz/Classes.java
@@ -2,6 +2,33 @@ package dev.andreamartz;
 
 public class Classes {
     public static void main(String[] args) {
+        Iphone iphone7Plus = new Iphone();
+        Iphone iphoneXMax = new Iphone();
 
+        // iphone7Plus
+        System.out.println(iphone7Plus.getModel());  // null
+        System.out.println(iphone7Plus.getPrice());  // 0.0
+        System.out.println(iphone7Plus.isWaterResistant());   // false
+
+        iphone7Plus.setModel("7+");
+        iphone7Plus.setPrice(200.00);
+        iphone7Plus.setWaterResistant(false);
+
+        System.out.println(" ");
+        System.out.println("7Plus:");  // 7Plus:
+        System.out.println(iphone7Plus.getModel());  // 7+
+        System.out.println(iphone7Plus.getPrice());  // 200.00
+        System.out.println(iphone7Plus.isWaterResistant());   // false
+
+        // iphoneXMax
+        iphoneXMax.setModel("X Max");
+        iphoneXMax.setPrice(1300.00);
+        iphoneXMax.setWaterResistant(true);
+
+        System.out.println(" ");
+        System.out.println("XMax:");  // X Max:
+        System.out.println(iphoneXMax.getModel());  // X Max
+        System.out.println(iphoneXMax.getPrice());  // 1300.00
+        System.out.println(iphoneXMax.isWaterResistant());   // true
     }
 }
