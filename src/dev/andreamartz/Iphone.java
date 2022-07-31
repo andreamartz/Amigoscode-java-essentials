71fb74713e0490850334253ac5de5c26323e2149
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-07-31 12:26:38 -0500

    Classes

---
+++ src/dev/andreamartz/Iphone.java
@@ -0,0 +1,10 @@
+package dev.andreamartz;
+
+public class Iphone {
+    // the following three variables are called `properties`  on the class
+
+    // `private` is used in encapsulation, to hide data from the outside world
+    private String model;
+    private double price;
+    private boolean isWaterResistant;
+}
