20fc25c88322e630f3d8cefd958f1b38c18b688b
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2022-08-03 16:50:26 -0500

    Available Date Methods

--- src/dev/andreamartz/Main.java
+++ src/dev/andreamartz/Main.java
@@ -2,14 +2,25 @@ package dev.andreamartz;
 
 import java.awt.*;
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
         LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Australia/West"));
-
+        LocalDateTime now = LocalDateTime.now();
 
 //        ZoneId.getAvailableZoneIds().forEach(System.out::println);
+
+        LocalDate someDate = LocalDate.of(2000, Month.SEPTEMBER, 1);
+
+        System.out.println(someDate.minus(2, ChronoUnit.DECADES));
+
+        System.out.println(someDate.isAfter(now.toLocalDate()));
+        System.out.println(someDate.getYear());
+        System.out.println(someDate.getMonth());
+        System.out.println(someDate.getDayOfWeek());
+
 
         System.out.println(localDate);
         System.out.println(localDateTime);
